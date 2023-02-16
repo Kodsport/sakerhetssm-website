b62c1146c5245d87b19679490a2e6d6e077fbac7
mateuszdrwal <drwal.mateusz@gmail.com>
2023-02-16 22:39:04 +0100

    🤫^2

--- src/pages/snyggt.js
+++ src/pages/snyggt.js
@@ -4,8 +4,8 @@ import MainLayout from "../components/MainLayout"
 const SnyggtPage = () => {
   return (
     <MainLayout>
-      <h1>Oj, du var snabb!</h1>
-      <p>Grattis till den korrekta lösningen! Vi har dock inte hunnit lägga ut något vettigt på den här sidan än, så kom tillbaka närmre kvalet.</p>
+      <h1>Snyggt!</h1>
+      <p>Grattis till den korrekta lösningen! Du har redan nu tjänat in 100 poäng på tävlingen. Registrera dig på <a href="https://ctf.sakerhetssm.se">https://ctf.sakerhetssm.se</a> (skapa gärna ett lag med några kompisar!) och vänta tills kvalet börjar den 10 mars klockan 20:00. Skicka sedan in lösningen, alltså ordet <code>snyggt</code> på uppgiften <code>Länkstrul</code> för att få dina poäng.</p>
     </MainLayout>
   )
 }
