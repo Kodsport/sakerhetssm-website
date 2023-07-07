0c19a51a56778f46259591af941a01274afd7aa4
nnewram <marwenn02@gmail.com>
2023-07-07 14:15:03 +0200

    add jul 2023

---
+++ src/pages/monthly/jul-2023/index.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+import MonthlyLayout from "../../../components/MonthlyLayout"
+import svinstian from "../../../images/svinstian.png"
+import style from "../../../styles/Util.module.scss"
+
+const FebMar2022Page = () => {
+  return (
+    <MonthlyLayout>
+      <h1>Månadens problem - Juli 2023</h1>
+      <p>Jätte Wiktigt att Tänka på säkerheten! Servern ligger på <code>http://35.228.72.74:3000</code>.</p>
+    </MonthlyLayout>
+  )
+}
+
+export default FebMar2022Page
