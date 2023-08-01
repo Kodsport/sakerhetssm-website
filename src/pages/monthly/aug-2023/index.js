c49e5f7a8eb6ed554a5f45ce0754c98511f5abc3
nnewram <marwenn02@gmail.com>
2023-08-01 11:19:02 +0200

    Adding augusti 2023

---
+++ src/pages/monthly/aug-2023/index.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+import MonthlyLayout from "../../../components/MonthlyLayout"
+
+const Aug2023Page = () => {
+  return (
+    <MonthlyLayout>
+      <h1>PyKlock 2000 - Månadens problem, Augusti 2023</h1>
+      <p>Author: Valter Wik</p>
+      <p>Som jag har längtat, och äntligen är den här!! Min alldeles egna <a href="/monthly/aug-2023/pyklock.py">PyKlock™</a> nu kan jag äntligen veta vad Tiden™ är!</p>
+      <p><code>nc 35.228.72.74 50000</code></p>
+    </MonthlyLayout>
+  )
+}
+
+export default Aug2023Page
